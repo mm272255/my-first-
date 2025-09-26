d9a82ffa5179a26a07e17b8ccf51ca08a3d6cf76
mm272255 <1558135319@qq.com>
2025-09-26 14:45:19 +0800

    Add files via upload

---
+++ 顺序表-将奇数移到偶数前面-区间划分法.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+#define Maxsize 100
+typedef struct S{
+	int data[Maxsize];
+	int length;
+}SqList;
+void IniList(SqList*&L){
+	L=(SqList*)malloc(sizeof(SqList));
+	L->length=0;
+}
+void DispList(SqList*L){
+	for(int i=0;i<L->length;++i){
+		cout<<L->data[i]<<" ";
+	}
+	cout<<endl;
+}
+void CreateList(SqList * &L,int a[],int n){
+	int i=0,k=0;
+	L=(SqList*)malloc(sizeof(SqList));
+	while(i<n){
+		L->data[i]=a[i];
+		k++,i++;
+	}
+	L->length=k;
+}
+void movel2(SqList*&L){
+	int i=-1;
+	for(int j=0;j<L->length;++j){
+		if(L->data[j]%2!=0){
+			i++;
+			if(i!=j){
+				swap(L->data[i],L->data[j]);
+			}
+		}
+	}
+}
+int main(){
+	SqList*L;
+	int b[]={1,4,3,6,7,2,9,8};
+	int n=sizeof(b)/sizeof(b[0]);
+	IniList(L);
+	CreateList(L,b,n);
+	DispList(L);
+	movel2(L);
+	DispList(L);
+}
